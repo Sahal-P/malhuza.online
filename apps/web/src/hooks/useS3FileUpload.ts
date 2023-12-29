ae6bd407fcefb9a6f27a7e55bc63765c8ea7a2de
Sahal-P <sahlcyprus@gmail.com>
2023-12-29 20:29:05 +0530

    add: s3 file remove mutation && updated upload mutation with 'key' param

--- apps/web/src/hooks/useS3FileUpload.ts
+++ apps/web/src/hooks/useS3FileUpload.ts
@@ -23,10 +23,10 @@ export const useCoverImage = create<CoverImageStore>((set) => ({
 
 
 const useS3FileUpload = () => {
-  return useMutation<S3Response, AxiosError, { file: File; id: string }>({
-    mutationFn: async ({ file, id }: {file: File, id:string}) => {
+  return useMutation<S3Response, AxiosError, { file: File; id: string; key?: string; }>({
+    mutationFn: async ({ file, id, key }) => {
       // If a file is provided, upload it to S3 first
-      const {data: s3} = await axios.get("api/s3/presigned-url", {params: {"file_key": file.name}});
+      const {data: s3} = await axios.get("api/s3/presigned-url", {params: {"file_key": key ? key : file.name}});
       const axiosWithoutInterceptor = axios.create();
       if (s3.presigned_url) {
         const {status} = await axiosWithoutInterceptor.put(s3.presigned_url, file , {headers: {"Content-Type": "multipart/form-data"}});
@@ -55,6 +55,35 @@ const useS3FileUpload = () => {
         } else {
           // Handle other types of errors
           toast.error("Failed to Upload", { duration: 1000 });
+        }
+        console.log(error);
+        
+      },
+  });
+};
+
+export const useS3FileRemove = () => {
+  return useMutation({
+    mutationFn: async ({ id }: { id:string }) => {
+      // If a file is provided, upload it to S3 first
+      const {data} = await axios.patch("api/s3/remove-cover-image/", {id});
+      console.log(data);
+      
+      return data
+      
+    },
+    onSuccess: () => {
+      // Handle success
+      toast.success("removed successfully", { duration: 1000 });
+    },
+    onError: (error: AxiosError) => {
+        // Custom error handling based on AxiosError
+        if (axios.isAxiosError(error)) {
+          // Access specific properties of AxiosError, e.g., error.response?.status
+          toast.error(`Failed to remove: ${error.message}`, { duration: 1000 });
+        } else {
+          // Handle other types of errors
+          toast.error("Failed to remove", { duration: 1000 });
         }
         console.log(error);
         
