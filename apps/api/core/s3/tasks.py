0605e7f07366be6e83ad541c378dacfd7dab894e
Sahal-P <sahlcyprus@gmail.com>
2023-12-29 19:26:52 +0530

    add: celery task to remove s3 object

--- apps/api/core/s3/tasks.py
+++ apps/api/core/s3/tasks.py
@@ -1,9 +1,7 @@
-# tasks.py
-
 from celery import shared_task
 from document.models import Document
 from django.core.files.base import ContentFile
-
+from utils.s3 import get_s3_client
 import blurhash
 from PIL import Image as PILImage
 
@@ -13,9 +11,20 @@ def encodeImageToBlurHash(id, image_data):
         image = PILImage.open(ContentFile(image_data))
         hash = blurhash.encode(image=image, x_components=4, y_components=3)
         instance = Document.objects.get(id=id)
-        instance.coverImageBlurHash = hash
+        instance.cover_image_blurhash = hash
         instance.save()
+        print("Blur hash completed successfully")
     except:
+        print("Blur hash error occured")
         pass
 
-    print("Blur hash completed successfully")
+
+@shared_task
+def removeS3object(object_key, bucket_name):
+    try:
+        s3 = get_s3_client()
+        s3.delete_object(Bucket=bucket_name, Key=object_key)
+        print("s3 object removed successfully")
+    except:
+        print("s3 object error occured during removal")
+        pass
