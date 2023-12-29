6ccccf4d70422fc56b3b2dbbb183907fbaf13276
Sahal-P <sahlcyprus@gmail.com>
2023-12-29 19:32:07 +0530

    add: remove-cover-image api url

--- apps/api/core/core/urls.py
+++ apps/api/core/core/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from s3.views import S3PresignedUrl, BlurHash
+from s3.views import S3PresignedUrl, BlurHash, RemoveCoverImageAPIView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
     path('api/docs/', include('document.urls')),
     path('api/s3/presigned-url', S3PresignedUrl.as_view(), name="s3-presigned-url"),
     path('api/s3/blurhash', BlurHash.as_view(), name="s3-blurhash"),
+    path('api/s3/remove-cover-image/', RemoveCoverImageAPIView.as_view(), name='remove-cover-image'),
 ]
