48e4ff2d04b84e4a3fa9d1852233a277831a7730
Sahal-P <sahlcyprus@gmail.com>
2023-12-30 22:36:15 +0530

    add: Editor component setup via blocknote lib

---
+++ apps/web/src/pages/main/components/Editor.tsx
@@ -0,0 +1,54 @@
+import { FC, useEffect } from "react";
+import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
+import { BlockNoteView, useBlockNote } from "@blocknote/react";
+import "@blocknote/core/style.css";
+import content from './TestContent'
+import { useEditorTheme, useTheme } from "@/components/common/ThemeProvider";
+
+interface EditorProps {
+  onChange: (value: string) => void;
+  initialContent?: string;
+  editable?: boolean;
+}
+
+const Editor: FC<EditorProps> = ({ onChange, initialContent, editable }) => {
+
+    const {theme} = useTheme()
+    const {lightTheme, darkTheme, systemTheme} = useEditorTheme()
+
+const handleUpload = (file: File) => {
+    console.log(file);
+    const upload = new Promise<string>((resolve, reject) => {
+        setTimeout(() => {
+          resolve("https://s3.eu-north-1.amazonaws.com/async-await.online/media/images/malhuza/jwt_middleware_2%20%282%29%20%281%29.png");
+        }, 300);
+      });
+    return upload
+}
+
+
+console.log(typeof(content))
+console.log(typeof(initialContent))
+  const editor: BlockNoteEditor = useBlockNote({
+    editable,
+    initialContent: initialContent
+      ? JSON.parse(initialContent) as PartialBlock[]
+      : undefined,
+    onEditorContentChange: (editor) => {
+      console.log(editor);
+      console.log(JSON.stringify(editor.topLevelBlocks, null, 2));
+      
+
+      onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
+    },
+    uploadFile: handleUpload
+  });
+  useEffect(() => {}, []);
+  return (
+    // <div>
+      <BlockNoteView className="dark:bg-document_bg" editor={editor} theme={theme === "dark" && darkTheme || theme === "light" && lightTheme || systemTheme} />
+    // </div>
+  );
+};
+
+export default Editor;
