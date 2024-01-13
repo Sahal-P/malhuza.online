2cd97808068ec907693e174cee81bd32b3eb498b
Sahal-P <sahlcyprus@gmail.com>
2024-01-13 23:48:49 +0530

    removed: spinner div

--- apps/web/src/pages/main/Main.tsx
+++ apps/web/src/pages/main/Main.tsx
@@ -46,14 +46,10 @@ const Main: FC<MainProps> = () => {
    <div className="h-screen flex dark:bg-[#1F1F1F] overflow-hidden">
     <Navigation/>
       <main className="flex-1 h-full overflow-y-auto bg-[#ffffff] dark:bg-document_bg scrollbar-thin scrollbar-thumb-secondary">
-        {/* <div className="w-full h-full  flex justify-center items-center z-[9999999999]">
-        <Spinner size={"lg"}/>
-        </div> */}
         <SearchCommand />
         <SettingsModal/>
         <CoverImageModal/>
         <Outlet/>
-      {/* <ChatBox/> */}
       </main>
    </div>
    </UserProvider>
