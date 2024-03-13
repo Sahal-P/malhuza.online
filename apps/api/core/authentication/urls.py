30f5cd9f731a8158e15ef2a0c7c9b0abd43a1e49
Sahal-P <sahlcyprus@gmail.com>
2024-03-13 23:59:46 +0530

    added user details view

--- apps/api/core/authentication/urls.py
+++ apps/api/core/authentication/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("login/", views.LoginView.as_view(), name="rest_login"),
     path("logout/", views.LogoutAPIView.as_view(), name="rest_logout"),
     path("user/", views.CustomUserDetailsView.as_view(), name="rest_user_details"),
+    path("users/", views.CustomUsersDetailsView.as_view(), name="rest_users_details"),
     path("token/verify/", views.TokenVerifyView.as_view(), name="token_verify"),
     path("token/refresh/", views.get_refresh_view().as_view(), name="token_refresh"),
     path("google/", GoogleSocialAuthView.as_view(), name="google_login"),
